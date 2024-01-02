553ebf42e9270e00dbf45fb4aba03a279b835d77
reenact_l <77737814+jhlee343@users.noreply.github.com>
2024-01-02 18:41:53 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -1,82 +1,88 @@
 import React, {Component} from "react";
-//class component로 만들어보자..힝 아무것도 몰라 너무 어려워요 시발 리액트 포기햇는데 나한테 왜그래요ㅠ
 /*
 1. 전체 코드 한눈에 볼 수 있게 만들기
-1.1 코드 전체 출력    12.31.20.01
+1.1 코드 전체 출력  
 1.2 줄마다 코드 라인에 맞는 들여쓰기 진행
-1.3 2.2를 수행하면 해당한 문장이 bold체로 변환
+1.3 2.2를 수행하면 해당한 문장 변환
 
 2. 현재 타이핑하는 문장 불러와서 출력하기
 2.1 실시간 타이핑 내용을 문장과 비교해서 출력
+
+3. 정확도 / 타수 측정
 */
+
+let [title, setTitle] = useState(['나는 화랑이다', '오늘은 목요일', '지금 날씨가 맑아']);
+let [input, setInput] = useState('');
+
 import "./App.css";
 export default class App extends Component {
+
   state = {
     doSentence : [
       {
         id:"1",
         title : "import java.util.*",
-        completed :true
+        completed :false
       },
       {
         id:"2",
         title : "import java.io.*",
-        completed :true
+        completed :false
       },
       {
         id:"3",
         title : "public class Main{",
-        completed :true
+        completed :false
       },
       {
         id:"4",
-        title : "public stati void main(String[] args) throws IOEXCEPTION{",
-        completed :true
+        title : "public stati void main(String[] args) throws IOException{",
+        completed :false
       },
       {
         id:"5",
         title : "BufferedReader br = new BufferedReader(new InputStreamReader));",
-        completed :true
+        completed :false
       },
       {
         id:"6",
         title : "int n = Integer.parseInt(br.readLine());",
-        completed :true
+        completed :false
       },
       {
         id:"7",
         title : "int[][] map = new int[n][n];",
-        completed :true
+        completed :false
       },
       {
         id:"8",
         title : "long[][] dp = new long[n][n];",
-        completed :true
+        completed :false
       },
       {
         id:"9",
         title : "StringTokenizer st;",
-        completed :true
+        completed :false
       },
       {
         id:"10",
         title : "for(int i = 0; i < n; i++){",
-        completed :true
+        completed :false
       },
       {
         id:"11",
         title : "st = new StringTokenizer(br.readLine());",
-        completed :true
+        completed :false
       },
       {
         id:"12",
         title : "for(int j =0; j<n ; j++){",
-        completed :true
+        completed :false
       },
       {
         id:"13",
         title : "map[i][j] = Integer.parseInt(st.nextToken());",
-        completed :true
+        completed :false
       },
     ],
     value : "",
@@ -85,19 +91,27 @@ export default class App extends Component {
   handleChange =(e) => {
     console.log('e',e.target.value);
     this.setState({value: e.target.value});
+    //문장 변환해야함
+    
   };
 
   handleSubmit=(e) => {
     e.preventDefault();
-
+    
+    //값 비교해야함
   }
-
-
-  // handleClick =(id) => {
-  //   let newData = this.state.doSentence.filter((data)=>data.id !==id);
-  //   console.log('newData',newData);
-  //   this.setState({doSentence: newData});
-  // }
+  
+  //enter치면 현재 문장과 타이핑 문장 비교해서 볼드 표시 
+  handleOnKeyPress = (e) => {
+    if(e.key='enter'){
+     this.handleSubmit(); 
+    }
+  }
+  //  handleClick =(id) => {
+  //    let newData = this.state.doSentence.filter((data)=>data.id !==id);
+  //    console.log('newData',newData);
+  //    this.setState({doSentence: newData});
+  //  }
 
   render(){
     return(
@@ -110,7 +124,7 @@ export default class App extends Component {
           {this.state.doSentence.map(data =>(
             <div className="total-code" key={data.id}>  
               <div>
-                {data.id +" "}
+                {data.id +". "}
                 {data.title}
                 <hr></hr>
                 {/* <button onClick={()=>this.handleClick(data.id)}/> */}
@@ -120,26 +134,37 @@ export default class App extends Component {
         </div>
 
 
-        <div className="typingContainer">
-          <div className="senctence-Block">
-            
-          </div>
-          
-            <form style={{ display: 'flex'}} onSubmit ={this.handleSubmit}>
-              <input 
+        <div className="typingContainer"> 
+          <div className="now-sentence">
+            {/*
+            해당하는 문장만 출력하게..
+          */}
+              {this.state.doSentence[0].title}
+              
+            {/* {this.state.doSentence.map(data =>(
+              <div className="total-code" key={data.id}>  
+                <div>
+                  {data.title}
+                  <hr></hr>
+                </div>
+             </div>
+            ))} */}
+            </div>
+
+            <form style={{ display: 'flex'}}>
+              <input onChange={(e)=>{
                 type="text" 
-                name="value" 
-                style={{flex:'10', padding:'5px'}}
-                placeholder={this.state.doSentence.title}
-                value={this.state.doSentence.value}
-                onChange={this.handleChange} 
+                placeholder="Typing.."
+                setInput(e.target.value);
+              }}
                 />
-                <input
-                type="submit"
-                value = "enter"
-                className="btn"
-                style={{flex: 1}}
-                />
+
+                <button onClick={()=>{
+                let copytitle = [...title]
+                copytitle.push(input);
+                 setTitle(copytitle)
+                 }}>글작성
+                 </button>
             </form>
           
         </div>
